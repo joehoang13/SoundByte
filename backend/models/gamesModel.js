fb99350f6787a3cf6a4eb54d23a30666e4d800a7
Joe Hoang <jahstudentedu@gmail.com>
2025-06-04 15:44:21 -0400

    Set up barebones for backend: routes, controllers, models

---
+++ backend/models/gamesModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const gameSchema = mongoose.Schema({
+    players: String,
+    answers: String,
+    timeStamps: String,
+    scores: String,
+})
+
+module.exports = mongoose.model('Game', gameSchema);
---
+++ backend/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/usersController');
+
+router.get('/', userController.getUserByEmail);
+router.get('/dummy', userController.getUserDummy);
+
+module.exports = router;
