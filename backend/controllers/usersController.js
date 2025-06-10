29db58646aa8d30cdd8e411de4ef01aabe152a0f
Joe Hoang <jahstudentedu@gmail.com>
2025-06-10 17:02:42 -0400

    Update prettier

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,29 +1,29 @@
-const User = require('../models/usersModel');
+const User = require('../models/usersModel')
 
-exports.getUserDummy = async(req, res) => {
-    const user = {
-        _id: '60f7e3b5d6e2f1a5b2c8a9f0',
-        email: 'dummyuser@example.com',
-        authMethod: 'guest',
-        scoreHistory: 1000000,         
-      };
-    
-      res.json(user);
+exports.getUserDummy = async (req, res) => {
+  const user = {
+    _id: '60f7e3b5d6e2f1a5b2c8a9f0',
+    email: 'dummyuser@example.com',
+    authMethod: 'guest',
+    scoreHistory: 1000000,
+  }
+
+  res.json(user)
 }
 
-exports.getUserByEmail = async(req, res) => {
-    const { email } = req.params;
+exports.getUserByEmail = async (req, res) => {
+  const { email } = req.params
 
-    try {
-        const user = await User.findOne({ email });
-    
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
-        }
-    
-        res.json(user);
-      } catch (error) {
-        console.error('Error fetching user by email: ', error);
-        res.status(500).json({ message: 'Internal server error' });
-      }
-};
+  try {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    res.json(user)
+  } catch (error) {
+    console.error('Error fetching user by email: ', error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/usersController');
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/usersController')
 
-router.get('/', userController.getUserByEmail);
-router.get('/dummy', userController.getUserDummy);
+router.get('/', userController.getUserByEmail)
+router.get('/dummy', userController.getUserDummy)
 
-module.exports = router;
+module.exports = router
