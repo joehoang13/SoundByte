3c894d7de3514123c714e2d2dea61320409386fc
mizzie221 <mi852768@ucf.edu>
2025-06-26 08:39:18 -0400

    Added PlayerStats schema

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usersController');
 const User = require('../models/Users');
+const Snippet = require('../models/Snippet');
+const GameSession = require('../models/GameSession');
+const PlayerStats = require('../models/PlayerStats');
 
 router.get('/', userController.getUserByEmail);
 router.get('/dummy', userController.getUserDummy);
 
 module.exports = router;
 
+// test code for Users.js, remove at any time
 router.post('/test', async (req, res) => {
     try {
       console.log('POST /api/users/test route hit');
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-console.log("Loaded MONGODB_URI:", process.env.MONGODB_URI);
 
+// Load models
+require('./models/Users');
+require('./models/Snippet');
+require('./models/GameSession');
+require('./models/PlayerStats');
+
+
+console.log("Loaded MONGODB_URI:", process.env.MONGODB_URI);
 
 const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('✅ MongoDB connected'))
-.catch((err) => console.error('❌ MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
 
 // Routes
 const userRoutes = require('./routes/users');
